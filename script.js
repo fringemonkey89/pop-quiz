a778aa4ed45e72d709452a783318bc1c9cc12997
fringemonkey89 <markomajeticjr@gmail.com>
2023-07-27 11:14:21 -0400

    added code to index and script file

---
+++ script.js
@@ -0,0 +1,41 @@
+const questions = [{
+    q: "What does HTML stand for?",
+    a: [{text:"Hyper Text Mark-up Language", isCorrect:true},
+        {text:"Hyper Text Mark-up Language", isCorrect:false},
+        {text:"Hyper Text Mark-up Language", isCorrect:false},
+        {text:"Hyper Text Mark-up Language", isCorrect:false}
+    ]
+},
+
+{    q: "What does HTML stand for?",
+     a: [{text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:true},
+    {text:"Hyper Text Mark-up Language", isCorrect:false}
+]
+},
+
+{    q: "What does HTML stand for?",
+     a: [{text:"Hyper Text Mark-up Language", isCorrect:true},
+    {text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:false}
+]
+},
+
+{    q: "What does HTML stand for?",
+     a: [{text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:true}
+]
+},
+
+{    q: "What does HTML stand for?",
+     a: [{text:"Hyper Text Mark-up Language", isCorrect:true},
+    {text:"Hyper Text Mark-up Language", isCorrect:true},
+    {text:"Hyper Text Mark-up Language", isCorrect:false},
+    {text:"Hyper Text Mark-up Language", isCorrect:false}
+]
+}
+]
