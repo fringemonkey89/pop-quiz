bf16be2d5f9ab6ea99d009aa0c563bcdbf9ae9c2
fringemonkey89 <markomajeticjr@gmail.com>
2023-08-02 10:31:49 -0400

    fixed code for javascript, added changes to html and css

--- script.js
+++ script.js
@@ -57,29 +57,36 @@ const questions = [{
 let currentQuestion = 0;
 let score = 0;
 
+
 const button = document.getElementById("btn");
 button.addEventListener("click", checkAnswer);
 
 const subScore = document.getElementById("submitScore")
 subScore.addEventListener("click", submitInitial);
+subScore.textContent = "Submit Initials";
 
+const restart = document.getElementById('ReplayQuiz');
+restart.addEventListener('click', resetQuiz);
 function submitInitial (){
+    restart.style.display = 'block';
+    restart.textContent = 'Play Again?';
     var nameInitials = document.getElementById('initialInput');
     var userScore = score;
     var userInfo = {
     userInitial: nameInitials.value.trim(),
     userfinalScore: userScore
     };
-
-    localStorage.setItem('user', JSON.stringify(userInfo));
+    nameInitials.textContent = '';
+    localStorage.setItem('user', JSON.stringify(userInitial));
+    document.getElementById('record').textContent = localStorage.getItem('');
     
-   document.getElementById('record').textContent = localStorage.getItem('userInitial');
-   
 }
-
+window.onload = function(){
+    window.alert("The quiz is about to start hot-shot and you have 30 seconds or less to complete it")
+}
 function loadQuestion (){
-    const Question = document.getElementById("ques")
-    const option = document.getElementById("opt")
+    const Question = document.getElementById("ques");
+    const option = document.getElementById("opt");
 
     Question.textContent = questions[currentQuestion].q;
 
@@ -93,8 +100,9 @@ function loadQuestion (){
         choice.type = "radio";
         choice.name = "answer";
         choice.value = i;
- 
+        
         choicesLabel.textContent = questions[currentQuestion].a[i].text;
+       console.log(choicesLabel.textContent = questions[currentQuestion].a[i].text)
         choicesContainer.appendChild(choice);
         choicesContainer.appendChild(choicesLabel);
         option.appendChild(choicesContainer);
@@ -107,10 +115,11 @@ function nextQuestion(){
         currentQuestion++;
         loadQuestion();
     } else {
-        document.getElementById("opt").remove()
-        document.getElementById("ques").remove()
-        document.getElementById("btn").remove()
-        clearTimeout(timerId);
+        document.getElementById("opt").style.display = "none";
+        document.getElementById("ques").style.display = "none";
+        document.getElementById("btn").style.display = "none";
+        timeLeft=0;
+        timeElem.innerHTML = timeLeft + ' seconds remaining';
         loadScore();
         const intials = document.getElementById("initialInput").style.display = "block";
         const subScore = document.getElementById("submitScore").style.display = "block";
@@ -120,13 +129,15 @@ function nextQuestion(){
 function checkAnswer() {
     const selectedAnswer = parseInt(document.querySelector('input[name="answer"]:checked').value);
     const resOutcome = document.getElementById("outcome");
-
+    
     if (questions[currentQuestion].a[selectedAnswer].isCorrect) {
         resOutcome.textContent = "correct answer";
         score++;
        nextQuestion();
-    } else {
-        resOutcome.textContent = "incorrect answer";  
+    } else{
+        resOutcome.textContent = "incorrect answer"; 
+        timeLeft -= 5;
+        timeElem.innerHTML = timeLeft + ' seconds remaining';
         nextQuestion();
     }
 }
@@ -135,3 +146,24 @@ function loadScore() {
     totalScore.textContent = "You scored " + score + " out of "  + questions.length;
 }
 
+function resetQuiz(){
+    timeLeft = 30;
+    currentQuestion = 0;
+    score = 0;
+    document.getElementById("opt").style.display = "block";
+    document.getElementById("ques").style.display = "block";
+    document.getElementById("btn").style.display = "block";
+    const resOutcome = document.getElementById("outcome");
+    resOutcome.textContent = "";
+    var nameInitials = document.getElementById('initialInput').style.display = 'none';
+    restart.style.display = 'none';
+    subScore.style.display = 'none';
+    hideScore();
+}
+
+function hideScore(){
+    score = 0;
+    const totalScore = document.getElementById("your_score");
+    totalScore.textContent = "";
+
+}
